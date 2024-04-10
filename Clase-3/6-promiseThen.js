c7065bde7a4ee754f54d24ded45da214da02eb05
MatiasDuro-ORT <matias.duro@ort.edu.ar>
2024-04-09 21:04:53 -0300

    clase 3 async

---
+++ Clase-3/6-promiseThen.js
@@ -0,0 +1,24 @@
+const obtenerUsuario = () => {
+
+    return fetch('https://randomuser.me/api/')
+    .then((respuesta) => {
+        return respuesta.json()
+    })
+    .then((datos) => {
+        const usuario = datos.results[0]
+        return usuario
+    })
+
+}
+
+
+const datos = obtenerUsuario()
+                .then((user) =>{
+                    // console.log(user);
+
+                    // console.log('Nombre: '+ user.name.first + " " + user.name.last);
+                    console.log(`Nombre: ${user.name.first} ${user.name.last}`);
+                    console.log(`Email: ${user.email}`);
+                })
+
+// console.log(datos);
---
+++ Clase-3/7-promiseAsyncAwait.js
@@ -0,0 +1,29 @@
+const cargarUsuario = async () => {
+    const respuesta = await fetch('https://randomuser.me/api/')
+
+    const datos = await respuesta.json()
+
+    
+
+    const usuario = datos.results[0]
+
+
+    return usuario
+
+}
+
+// Async y Await
+
+async function funcionPadre(){
+    const datos = await obtenerUsuario()
+    
+    console.log(`Nombre: ${datos.name.first} ${datos.name.last}`);
+
+}
+
+funcionPadre()
+
+
+// funcion flecha y funcion tradicional
+// function () es lo mismo que () =>
+
